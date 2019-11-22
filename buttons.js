5410d1d13699d53428555619fe7c147ef3d6e093
donovanl64 <dlafonta@uvm.edu>
2019-11-22 12:09:34 -0500

    New Buttons

--- buttons.js
+++ buttons.js
@@ -1,14 +1,42 @@
-var bump = $("#bump");
-bump.click(function() {
-  console.log(bump.text());
-  if(bump.text() === "Bump") {
+//Button that bumps the light to full intensity
+var Bump = $("#bump");
+Bump.click(function() {
+  console.log(Bump.text());
+  if(Bump.text() === "Bump") {
     $.ajax({
       url: "/bump",
       type: "post",
       success: function(response) {
         console.log(response);
-        redbutton.text("Bump")
+        Bump.text("Bump")
       }
     })
   }
 }
+
+// Button that says "Nextcue"
+var Nextcue = $("#Nextcue");
+Nextcue.click(function() {
+  console.log(Nextcue.text());
+  if(Nextcue.text() === "Next") {
+    $.ajax({
+      url: "/nextCue",
+      type: "post",
+      success: function(response) {
+        console.log(response);
+        Nextcue.text("Next")
+      }
+    })
+  }
+}
+
+//button that controls the slider
+var value;
+$("#slider").roundSlider({
+  sliderType: "min-range",
+  change: function(){
+  var obj1 =  $("#slider").data("roundSlider");
+  value = obj1.getValue();
+  $.getJSON('/valueofslider', {
+    a:value
+  });
